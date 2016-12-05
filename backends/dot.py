ab3fd339089e23695af09f777f51a399fcb77506
Martin Bauer <martin.bauer@fau.de>
2016-12-05 16:22:17 +0100

    pystencils: display
    - colored dot output of AST
    - various display helpers used in documentation

--- backends/dot.py
+++ backends/dot.py
@@ -6,17 +6,18 @@ class DotPrinter(Printer):
     """
     A printer which converts ast to DOT (graph description language).
     """
-    def __init__(self, **kwargs):
+    def __init__(self, nodeToStrFunction, **kwargs):
         super().__init__()
+        self._nodeToStrFunction = nodeToStrFunction
         self.dot = Digraph(**kwargs)
         self.dot.quote_edge = lang.quote
 
     def _print_KernelFunction(self, function):
-        self.dot.node(repr(function))
+        self.dot.node(self._nodeToStrFunction(function), style='filled', fillcolor='#E69F00')
         self._print(function.body)
 
     def _print_LoopOverCoordinate(self, loop):
-        self.dot.node(repr(loop))
+        self.dot.node(self._nodeToStrFunction(loop), style='filled', fillcolor='#56B4E9')
         self._print(loop.body)
 
     def _print_Block(self, block):
@@ -24,18 +25,30 @@ class DotPrinter(Printer):
             self._print(node)
         parent = block.parent
         for node in block.children():
-            self.dot.edge(repr(parent), repr(node))
-            parent = node
+            self.dot.edge(self._nodeToStrFunction(parent), self._nodeToStrFunction(node))
+            #parent = node
 
     def _print_SympyAssignment(self, assignment):
-        self.dot.node(repr(assignment))
+        self.dot.node(self._nodeToStrFunction(assignment))
 
     def doprint(self, expr):
         self._print(expr)
         return self.dot.source
 
 
-def dotprint(ast, view=False, **kwargs):
+def __shortened(node):
+    from pystencils.ast import LoopOverCoordinate, KernelFunction, SympyAssignment
+    if isinstance(node, LoopOverCoordinate):
+        return "Loop over dim %d" % (node.coordinateToLoopOver,)
+    elif isinstance(node, KernelFunction):
+        params = [f.name for f in node.fieldsAccessed]
+        params += [p.name for p in node.parameters if not p.isFieldArgument]
+        return "Func: %s (%s)" % (node.functionName, ",".join(params))
+    elif isinstance(node, SympyAssignment):
+        return "Assignment: " + repr(node.lhs)
+
+
+def dotprint(ast, view=False, short=False, **kwargs):
     """
     Returns a string which can be used to generate a DOT-graph
     :param ast: The ast which should be generated
@@ -43,8 +56,28 @@ def dotprint(ast, view=False, **kwargs):
     :param kwargs: is directly passed to the DotPrinter class: http://graphviz.readthedocs.io/en/latest/api.html#digraph
     :return: string in DOT format
     """
-    printer = DotPrinter(**kwargs)
+    nodeToStrFunction = __shortened if short else repr
+    printer = DotPrinter(nodeToStrFunction, **kwargs)
     dot = printer.doprint(ast)
     if view:
         printer.dot.render(view=view)
     return dot
+
+if __name__ == "__main__":
+    from pystencils import Field
+    import sympy as sp
+    imgField = Field.createGeneric('I',
+                                   spatialDimensions=2, # 2D image
+                                   indexDimensions=1)   # multiple values per pixel: e.g. RGB
+    w1, w2 = sp.symbols("w_1 w_2")
+    sobelX = -w2 * imgField[-1, 0](1) - w1 * imgField[-1, -1](1) - w1 * imgField[-1, +1](1) \
+             + w2 * imgField[+1, 0](1) + w1 * imgField[+1, -1](1) - w1 * imgField[+1, +1](1)
+    sobelX
+
+    dstField = Field.createGeneric('dst', spatialDimensions=2, indexDimensions=0)
+    updateRule = sp.Eq(dstField[0, 0], sobelX)
+    updateRule
+
+    from pystencils.cpu import createKernel
+    ast = createKernel([updateRule])
+    print(dotprint(ast, short=True))

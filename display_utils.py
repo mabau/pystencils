7a34c76286b7c1aba9ee4638279bdfcf11ce1130
Jan Hönig <jan.hoenig@fau.de>
2017-12-22 16:27:55 +0100

    Added advst presentation
    Added DebugGui

--- display_utils.py
+++ display_utils.py
@@ -1,3 +1,6 @@
+import sys
+from PyQt5.QtWidgets import QWidget, QApplication, QTreeWidget, QTreeWidgetItem, QHBoxLayout
+from pystencils.astnodes import Block, LoopOverCoordinate, KernelFunction
 
 
 def toDot(expr, graphStyle={}):
@@ -26,6 +29,16 @@ def highlightCpp(code):
             """.format(pygments_css=HtmlFormatter().get_style_defs('.highlight'))))
     return HTML(highlight(code, CppLexer(), HtmlFormatter()))
 
+
+def debugGUI(ast):
+    app = QApplication.instance()
+    if app is None:
+        app = QApplication(sys.argv)
+    else:
+        print('QApplication instance already exists: %s' % str(app))
+    ex = DebugTree()
+    ex.insert_ast(ast)
+    app.exec_()
 
 # ----------------- Embedding of animations as videos in IPython notebooks ---------------------------------------------
 
@@ -155,3 +168,41 @@ def makeSurfacePlotAnimation(runFunction, frames=90, interval=30):
         return plot,
 
     return animation.FuncAnimation(fig, updatefig, interval=interval, frames=frames, blit=False)
+
+
+# -------------------- DEBUG GUI ------------------------
+
+
+class DebugTree(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.tree = QTreeWidget(self)
+        self.tree.setColumnCount(1)
+        self.tree.setHeaderLabel('repr')
+
+        hbox = QHBoxLayout()
+        hbox.stretch(1)
+        hbox.addWidget(self.tree)
+
+        self.setWindowTitle('Debug')
+        self.setLayout(hbox)
+        self.show()
+
+    def insert_ast(self, node, parent=None):
+        if parent is None:
+            parent = self.tree
+        if isinstance(node, Block):  # Blocks are represented with the tree structure
+            item = parent
+        else:
+            item = QTreeWidgetItem(parent)
+            item.setText(0, repr(node))
+
+        if node.func in [LoopOverCoordinate, KernelFunction]:
+            self.tree.expandItem(item)
+
+        for child in node.args:
+            self.insert_ast(child, item)
+

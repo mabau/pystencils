dd28d03121a2b88f9e6bbe84217e928d8913cb26
Stephan Seitz <stephan.seitz@fau.de>
2019-10-10 17:38:05 +0200

    Fix printing of sp.Infinity/sp.NegativeInfinity
    For sympy, oo s a number. So pystencils prints a type infinity a double
    INFINITY as INFINITY.0

---
+++ pystencils_tests/test_print_infinity.py
@@ -0,0 +1,22 @@
+import pytest
+
+import pystencils
+from sympy import oo
+
+
+@pytest.mark.parametrize('type', ('float32', 'float64', 'int64'))
+@pytest.mark.parametrize('negative', (False, 'Negative'))
+@pytest.mark.parametrize('target', ('cpu', 'gpu'))
+def test_print_infinity(type, negative, target):
+
+    x = pystencils.fields(f'x:  {type}[1d]')
+
+    if negative:
+        assignment = pystencils.Assignment(x.center, -oo)
+    else:
+        assignment = pystencils.Assignment(x.center, oo)
+    ast = pystencils.create_kernel(assignment, data_type=type, target=target)
+
+    ast.compile()
+
+    print(ast.compile().code)

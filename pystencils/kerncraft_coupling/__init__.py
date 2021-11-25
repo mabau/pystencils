2082ef802a38f828365a0bbe9467743c65a5eb78
Markus Holzer <markus.holzer@fau.de>
2021-11-25 11:34:56 +0000

    Removed kerncraft

--- pystencils/kerncraft_coupling/__init__.py
+++
@@ -1,4 +0,0 @@
-from .generate_benchmark import generate_benchmark, run_c_benchmark
-from .kerncraft_interface import KerncraftParameters, PyStencilsKerncraftKernel
-
-__all__ = ['PyStencilsKerncraftKernel', 'KerncraftParameters', 'generate_benchmark', 'run_c_benchmark']

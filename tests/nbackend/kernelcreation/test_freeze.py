7568c463e4a897a2645f095352a62741caab2001
Daniel Bauer <daniel.j.bauer@fau.de>
2024-03-27 16:42:16 +0100

    Add support for bitwise operators and integer division

--- tests/nbackend/kernelcreation/test_freeze.py
+++ tests/nbackend/kernelcreation/test_freeze.py
@@ -4,17 +4,36 @@ from pystencils import Assignment, fields
 
 from pystencils.backend.ast.structural import (
     PsAssignment,
+    PsBlock,
     PsDeclaration,
 )
 from pystencils.backend.ast.expressions import (
+    PsArrayAccess,
+    PsBitwiseAnd,
+    PsBitwiseOr,
+    PsBitwiseXor,
     PsExpression,
-    PsArrayAccess
+    PsIntDiv,
+    PsLeftShift,
+    PsMul,
+    PsRightShift,
 )
 from pystencils.backend.constants import PsConstant
 from pystencils.backend.kernelcreation import (
     KernelCreationContext,
     FreezeExpressions,
     FullIterationSpace,
+)
+
+from pystencils.sympyextensions.integer_functions import (
+    bit_shift_left,
+    bit_shift_right,
+    bitwise_and,
+    bitwise_or,
+    bitwise_xor,
+    int_div,
+    int_power_of_2,
+    modulo_floor,
 )
 
 
@@ -63,9 +82,66 @@ def test_freeze_fields():
 
     zero = PsExpression.make(PsConstant(0))
 
-    lhs = PsArrayAccess(f_arr.base_pointer, (PsExpression.make(counter) + zero) * PsExpression.make(f_arr.strides[0]) + zero * one)
-    rhs = PsArrayAccess(g_arr.base_pointer, (PsExpression.make(counter) + zero) * PsExpression.make(g_arr.strides[0]) + zero * one)
+    lhs = PsArrayAccess(
+        f_arr.base_pointer,
+        (PsExpression.make(counter) + zero) * PsExpression.make(f_arr.strides[0])
+        + zero * one,
+    )
+    rhs = PsArrayAccess(
+        g_arr.base_pointer,
+        (PsExpression.make(counter) + zero) * PsExpression.make(g_arr.strides[0])
+        + zero * one,
+    )
 
     should = PsAssignment(lhs, rhs)
 
     assert fasm.structurally_equal(should)
+
+
+def test_freeze_integer_binops():
+    ctx = KernelCreationContext()
+    freeze = FreezeExpressions(ctx)
+
+    x, y, z = sp.symbols("x, y, z")
+    expr = bit_shift_left(
+        bit_shift_right(bitwise_and(x, y), bitwise_or(y, z)), bitwise_xor(x, z)
+    )
+
+    fexpr = freeze(expr)
+
+    x2 = PsExpression.make(ctx.get_symbol("x"))
+    y2 = PsExpression.make(ctx.get_symbol("y"))
+    z2 = PsExpression.make(ctx.get_symbol("z"))
+
+    should = PsLeftShift(
+        PsRightShift(PsBitwiseAnd(x2, y2), PsBitwiseOr(y2, z2)), PsBitwiseXor(x2, z2)
+    )
+
+    assert fexpr.structurally_equal(should)
+
+
+def test_freeze_integer_functions():
+    ctx = KernelCreationContext()
+    freeze = FreezeExpressions(ctx)
+
+    x2 = PsExpression.make(ctx.get_symbol("x", ctx.index_dtype))
+    y2 = PsExpression.make(ctx.get_symbol("y", ctx.index_dtype))
+    z2 = PsExpression.make(ctx.get_symbol("z", ctx.index_dtype))
+
+    x, y, z = sp.symbols("x, y, z")
+    asms = [
+        Assignment(z, int_div(x, y)),
+        Assignment(z, int_power_of_2(x, y)),
+        # Assignment(z, modulo_floor(x, y)),
+    ]
+
+    fasms = [freeze(asm) for asm in asms]
+
+    should = [
+        PsDeclaration(z2, PsIntDiv(x2, y2)),
+        PsDeclaration(z2, PsLeftShift(PsExpression.make(PsConstant(1)), x2)),
+        # PsDeclaration(z2, PsMul(PsIntDiv(x2, y2), y2)),
+    ]
+
+    for fasm, correct in zip(fasms, should):
+        assert fasm.structurally_equal(correct)
